94c0ac664fb7456f27d0fe124c1ed601e10035af
dwd-alt <iewlewroman85@gmail.com>
2026-01-18 01:05:20 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,13 @@
 from flask import Flask
-from flask_socketio import SocketIO
 import os
 
-socketio = SocketIO()
-
-
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, 
+                static_folder='static',
+                template_folder='templates')
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key')
-
+    
     from . import routes
     app.register_blueprint(routes.bp)
-
-    socketio.init_app(app)
-
+    
     return app
